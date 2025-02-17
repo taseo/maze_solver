9ead6cde2016c195cf4ce89075b8e4cf1bbcd413
taseo <aasaadesu@protonmail.ch>
2025-02-18 00:01:28 +0200

    Feature: add draw_move method to Cell class

--- cell.py
+++ cell.py
@@ -12,7 +12,17 @@ class Cell:
         self.has_top_wall = True
         self.has_bottom_wall = True
 
+        self.x_1 = 0
+        self.y_1 = 0
+        self.x_2 = 0
+        self.y_2 = 0
+
     def draw(self, x_1: int, y_1: int, x_2: int, y_2: int) -> None:
+        self.x_1 = x_1
+        self.y_1 = y_1
+        self.x_2 = x_2
+        self.y_2 = y_2
+
         if self.has_left_wall:
             self.__window.draw_line(
                 Line(
@@ -49,3 +59,11 @@ class Cell:
                 "black",
             )
 
+    def draw_move(self, to_cell: "Cell", undo: bool = False):
+        self.__window.draw_line(
+            Line(
+                Point((self.x_1 + self.x_2) // 2, (self.y_1 + self.y_2) // 2),
+                Point((to_cell.x_1 + to_cell.x_2) // 2, (to_cell.y_1 + to_cell.y_2) // 2),
+            ),
+            "gray" if undo else "red",
+        )
--- main.py
+++ main.py
@@ -6,8 +6,16 @@ from cell import Cell
 def main():
     win = Window(800, 600)
 
-    Cell(win).draw(50, 50, 100, 100)
-    Cell(win).draw(100, 100, 150, 150)
+    c_1 = Cell(win)
+    c_2 = Cell(win)
+    c_3 = Cell(win)
+
+    c_1.draw(50, 50, 100, 100)
+    c_2.draw(100, 100, 150, 150)
+    c_3.draw(150, 150, 200, 200)
+
+    c_1.draw_move(c_2)
+    c_2.draw_move(c_3, True)
 
     win.wait_for_close()
 
